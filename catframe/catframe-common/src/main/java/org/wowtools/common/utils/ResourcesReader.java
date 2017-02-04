8b2cadbf8fdad4b5befbbc965a4910c0ea162bec
刘雨 <liuyu@wowtools.org>
2017-02-04 10:15:25 +0800

    调整resources文件访问方式，不再支持优先根目录根目录下没有再jar，当项目被打包为jar时，只支持从根目录访问resources

--- catframe/catframe-common/src/main/java/org/wowtools/common/utils/ResourcesReader.java
+++ catframe/catframe-common/src/main/java/org/wowtools/common/utils/ResourcesReader.java
@@ -1,135 +1,126 @@
 package org.wowtools.common.utils;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 
+
+/**
+ * 文件资源读取工具类
+ *
+ * @author liuyu
+ * @date 2017/2/3
+ */
 public class ResourcesReader {
 
-	/**
-	 * 读取jar包中的资源文件
-	 * 
-	 * @param clazz
-	 * @param path
-	 *            资源相对路径，如：res.txt
-	 * @return
-	 */
-	public static String readInJarStr(Class<?> clazz, String path) {
-		InputStream is = null;
-		try {
-			is = readInJarStream(clazz, path);
-			byte b[] = new byte[is.available()];
-			is.read(b);
-			String res = new String(b);
-			return res;
-		} catch (Exception e) {
-			throw new RuntimeException("读取配置文件异常", e);
-		} finally {
-			if (null != is) {
-				try {
-					is.close();
-				} catch (IOException e) {
-				}
-			}
-		}
-	}
 
-	public static InputStream readInJarStream(Class<?> clazz, String path) {
-		try {
-			if (path.indexOf(0) != '/') {
-				path = "/" + path;
-			}
-			return clazz.getResourceAsStream(path);
-		} catch (Exception e) {
-			throw new RuntimeException("读取配置文件异常", e);
-		} finally {
-		}
-	}
+    /**
+     * 获得class所在根路径，若class被打成jar包，则返回jar所在根路径
+     *
+     * @param clazz 定位用的类
+     * @return 根目录路径
+     */
+    public static String getClassRootPath(Class<?> clazz) {
+        java.net.URL url = clazz.getProtectionDomain().getCodeSource()
+                .getLocation();
+        String filePath;
+        try {
+            filePath = java.net.URLDecoder.decode(url.getPath(), "utf-8");
+        } catch (Exception e) {
+            throw new RuntimeException("获取class路径失败", e);
+        }
+        if (filePath.endsWith(".jar"))
+            filePath = filePath.substring(0, filePath.lastIndexOf("/") + 1);
+        java.io.File file = new java.io.File(filePath);
+        filePath = file.getAbsolutePath();
+        return filePath;
+    }
 
-	/**
-	 * 获得根目录路径
-	 * @param clazz 使用的类
-	 * @return 根目录路径
-	 */
-	public static String getRootPath(Class<?> clazz) {
-		String basePath = clazz.getClassLoader().getResource("/") != null
-				? clazz.getClassLoader().getResource("/").getPath() : clazz.getResource("/").getPath();
-		return basePath;
-	}
+    /**
+     * 获得项目根路径
+     *
+     * @param clazz 定位用的类
+     * @return
+     */
+    public static String getRootPath(Class<?> clazz) {
+        // 检查用户传入的参数是否为空
 
-	/**
-	 * 读取项目中的资源
-	 * 
-	 * @param clazz 使用的类
-	 * @param path
-	 *            资源相对路径，如：res.txt
-	 * @return
-	 */
-	public static InputStream readInProjectStream(Class<?> clazz, String path) {
-		try {
-			String basePath = getRootPath(clazz);
-			return new FileInputStream(basePath + path);
-		} catch (Exception e) {
-			throw new RuntimeException("读取配置文件异常", e);
-		}
-	}
+        ClassLoader loader = clazz.getClassLoader();
+        // 获得类的全名，包括包名
+        String clsName = clazz.getName();
+        // 将类的class文件全名改为路径形式
+        String clsPath = clsName.replace(".", "/") + ".class";
 
-	/**
-	 * 读取项目中的资源
-	 * 
-	 * @param clazz
-	 * @param path
-	 *            资源相对路径，如：res.txt
-	 * @return
-	 */
-	public static String readInProjectStr(Class<?> clazz, String path) {
-		InputStream is = null;
-		try {
-			is = readInProjectStream(clazz, path);
-			byte b[] = new byte[is.available()];
-			is.read(b);
-			String res = new String(b);
-			return res;
-		} catch (Exception e) {
-			throw new RuntimeException("读取配置文件异常", e);
-		} finally {
-			if (null != is) {
-				try {
-					is.close();
-				} catch (IOException e) {
-				}
-			}
-		}
-	}
+        // 调用ClassLoader的getResource方法，传入包含路径信息的类文件名
+        java.net.URL url = loader.getResource(clsPath);
+        // 从URL对象中获取路径信息
+        String realPath = url.getPath();
+        // 去掉路径信息中的协议名"file:"
+        int pos = realPath.indexOf("file:");
+        if (pos > -1) {
+            realPath = realPath.substring(pos + 5);
+        }
+        // 去掉路径信息最后包含类文件信息的部分，得到类所在的路径
+        pos = realPath.indexOf(clsPath);
+        realPath = realPath.substring(0, pos - 1);
+        // 如果类文件被打包到JAR等文件中时，去掉对应的JAR等打包文件名
+        if (realPath.endsWith("!")) {
+            realPath = realPath.substring(0, realPath.lastIndexOf("/"));
+        }
+        java.io.File file = new java.io.File(realPath);
+        realPath = file.getAbsolutePath();
 
-	/**
-	 * 优先读取项目中的资源，读不到再读取jar中的
-	 * 
-	 * @param clazz
-	 * @param path
-	 * @return
-	 */
-	public static String readStr(Class<?> clazz, String path) {
-		try {
-			return readInProjectStr(clazz, path);
-		} catch (Exception e) {
-			return readInJarStr(clazz, path);
-		}
-	}
+        try {
+            realPath = java.net.URLDecoder.decode(realPath, "utf-8");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return realPath;
+    }
 
-	/**
-	 * 优先读取项目中的资源，读不到再读取jar中的
-	 * 
-	 * @param clazz
-	 * @param path
-	 * @return
-	 */
-	public static InputStream readStream(Class<?> clazz, String path) {
-		try {
-			return readInProjectStream(clazz, path);
-		} catch (Exception e) {
-			return readInJarStream(clazz, path);
-		}
-	}
+    /**
+     * 读取类所处根目录下的文件路径加上path的文件内容为String
+     *
+     * @param clazz 定位用的类
+     * @param path  类根路径下的相对路径
+     * @return
+     */
+    public static String readStr(Class<?> clazz, String path) {
+        InputStream is = null;
+        try {
+            is = readStream(clazz, path);
+            byte b[] = new byte[is.available()];
+            is.read(b);
+            String res = new String(b, "UTF-8");
+            return res;
+        } catch (Exception e) {
+            throw new RuntimeException("读取配置文件异常", e);
+        } finally {
+            if (null != is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
+
+    /**
+     * 读取类所处根目录下的文件路径加上path的文件内容为InputStream
+     *
+     * @param clazz 定位用的类
+     * @param path  类根路径下的相对路径
+     * @return
+     */
+    public static InputStream readStream(Class<?> clazz, String path) {
+        try {
+            String basePath = getRootPath(clazz);
+            return new FileInputStream(basePath + "/" + path);
+        } catch (Exception e) {
+            throw new RuntimeException("读取配置文件异常", e);
+        }
+    }
 
 }
